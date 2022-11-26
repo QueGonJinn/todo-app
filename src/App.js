807e9e947dd1096f15665160ca1dca79c1c21a87
Yury Molaikow <molialow13@gmail.com>
2022-11-26 11:06:09 +0300

    feat: minor changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { useState, useEffect } from 'react';
 import Todo from './components/Todo';
-import { HiOutlinePencilSquare } from 'react-icons/hi2';
 import { db } from './firebase';
-import { query, collection, onSnapshot } from 'firebase/firestore';
+import {
+	query,
+	collection,
+	onSnapshot,
+	updateDoc,
+	doc,
+	addDoc,
+	deleteDoc,
+} from 'firebase/firestore';
+import { HiOutlinePencilSquare } from 'react-icons/hi2';
 
 const style = {
 	bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#114b5f] to-[#88d498]`,
@@ -16,8 +24,21 @@ const style = {
 
 function App() {
 	const [todos, setTodos] = useState([]);
+	const [input, setInput] = useState('');
 
 	// Create todo
+
+	const createTodo = async (e) => {
+		e.preventDefault(e);
+		if (input === '') {
+			return;
+		}
+		await addDoc(collection(db, 'todos'), {
+			text: input,
+			completed: false,
+		});
+		setInput('');
+	};
 	// Read todo
 	useEffect(() => {
 		const q = query(collection(db, 'todos'));
@@ -31,24 +52,49 @@ function App() {
 		return () => unsubscribe;
 	}, []);
 	// Update todo
+	const toggleComplete = async (todo) => {
+		await updateDoc(doc(db, 'todos', todo.id), {
+			completed: !todo.completed,
+		});
+	};
 	// Delete todo
+	const deleteTodo = async (id) => {
+		await deleteDoc(doc(db, 'todos', id));
+	};
 
 	return (
 		<div className={style.bg}>
 			<div className={style.container}>
 				<h3 className={style.header}>Todo App</h3>
-				<form className={style.form}>
-					<input className={style.input} type="text" placeholder="Add Todo" />
+				<form onSubmit={createTodo} className={style.form}>
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						className={style.input}
+						type="text"
+						placeholder="Add Todo"
+					/>
 					<button className={style.button}>
 						<HiOutlinePencilSquare size={30} />
 					</button>
 				</form>
 				<ul>
 					{todos.map((todo, index) => {
-						return <Todo key={index} todo={todo} />;
+						return (
+							<Todo
+								key={index}
+								todo={todo}
+								toggleComplete={toggleComplete}
+								deleteTodo={deleteTodo}
+							/>
+						);
 					})}
 				</ul>
-				<p className={style.count}>You have 2 todos</p>
+				{todos.length < 1 ? null : (
+					<p className={style.count}>{`You have ${todos.length} ${
+						todos.length > 1 ? 'todos' : 'todo'
+					}`}</p>
+				)}
 			</div>
 		</div>
 	);
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,15 +10,27 @@ const style = {
 	button: `cursor-pointer flex items-center`,
 };
 
-function Todo({ todo }) {
+function Todo({ todo, toggleComplete, deleteTodo }) {
 	return (
 		<li className={todo.completed ? style.liComplete : style.li}>
 			<div className={style.row}>
-				<input type="checkbox" checked={todo.completed ? 'checked' : ''} />
-				<p className={style.text}>{todo.text}</p>
+				<input
+					onChange={() => toggleComplete(todo)}
+					type="checkbox"
+					checked={todo.completed ? 'checked' : ''}
+				/>
+				<p
+					onClick={() => toggleComplete(todo)}
+					className={todo.completed ? style.textComplete : style.text}
+				>
+					{todo.text}
+				</p>
 				<p>{todo.time}</p>
 			</div>
-			<button className={style.button}>
+			<button
+				onClick={() => deleteTodo(todo.id)}
+				className={style.button}
+			>
 				<FaRegTrashAlt />
 			</button>
 		</li>
